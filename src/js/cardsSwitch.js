452a1d6ec30fb4f7b778689f8ddb84a3eb6bfb03
Юрьев Александр <yuriev200@mail.ru>
2021-05-22 00:57:29 +0300

    added all js fetures for the cards: 1: more button works, 2: tabs are working well, 3: when user preses any tab page scrolls to start of tab section, 4: i added scrolling up button

--- src/js/cardsSwitch.js
+++
@@ -1,8 +0,0 @@
-const switchBtn = document.querySelector('.catalog-item__switchLink'),
-      cardContent = document.querySelector('.catalog-item__content'),
-      cardList = document.querySelector('.catalog-item__list');
-
-switchBtn.addEventListener('click', () => {
-    cardContent.classList.toggle('catalog-item__content_active');
-    cardContent.classList.toggle('catalog-item__list_active');
-});
--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+	let isName = false;
+
 	$('.carousel__inner').slick({
 		speed: 1200,
 		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
@@ -15,6 +17,46 @@ $(document).ready(function(){
 			}
 		]
 	});
+
+	(function($) {
+		$(function() {
+		  
+			$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+				$(this)
+				.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+				.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+			});
+			
+		});
+	})(jQuery);
+
+	$('.catalog-item__switchLink').each(function(i) {
+		$(this).on('click', (e) => {
+			e.preventDefault();
+			$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+			$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+
+			// Changes link name from 'MORE' to 'BACK'
+			if (isName) {
+				$('.catalog-item__switchLink').eq(i).text('ПОДРОБНЕЕ');
+				isName = false;
+			} else {
+				$('.catalog-item__switchLink').eq(i).text('НАЗАД');
+				isName = true;
+			}
+
+		});
+	});
+
+	// console.log('hello');
+
+	const tabs = document.querySelectorAll('.catalog__tab');
+	tabs.forEach(tab => {
+		tab.addEventListener('click', () => {
+			document.documentElement.scrollTop = 2700;
+		});
+	});
 });
 
-// console.log('hello');
+
+
